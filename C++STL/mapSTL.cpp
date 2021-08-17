ff425b79938f5c325c913c8d7b8ba06e1e9d5124
Sagnik Mitra <sagnikmitra123@gmail.com>
2021-08-17 16:42:35 +0530

    pair

--- C++STL/mapSTL.cpp
+++ C++STL/mapSTL.cpp
@@ -4,11 +4,16 @@ using namespace std;
 
 int main()
 {
+    //Map only stores unique keys
     //In case of map, it is sorted
+
+    //Unordered Map is the same just it doesn't store in any order, and it is O(1) in almost all the cases
+    //And O(n) in worst case
     map<int, string> m;
     m[1] = "Sagnik";
     m[2] = "Mitra";
     m[3] = "DS";
+    m.emplace(12, "Sa");
     m.insert({5, "CPP"});
     for (auto i : m)
     {
@@ -25,6 +30,15 @@ int main()
     {
         cout << (*i).first << endl;
         exit(0);
+    }
+    // m.erase(m.begin(), m.end());
+    for (auto itr : m)
+    {
+        cout << itr.first << " " << itr.second << endl;
+    }
+    for (auto itr = m.begin(); itr != m.end(); it++)
+    {
+        cout << (*itr).first << " " << itr->second << endl;
     }
     return 0;
 }
---
+++ C++STL/pairSTL.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    pair<pair<int, int>, int> pr = {{1, 2}, 2};
+    cout << pr.first.first << endl;
+    cout << pr.first.second << endl;
+    cout << pr.second << endl;
+    return 0;
+}
