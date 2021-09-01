8594c5f36144513aff3ed0f18288612860fe8782
Sagnik Mitra <sagnikmitra123@gmail.com>
2021-09-01 23:54:57 +0530

    hackercup_problem1_fc main_for

--- facebook_hackercup_problem1.cpp
+++ facebook_hackercup_problem1.cpp
@@ -5,11 +5,15 @@ int main(){
     map<string,int> consonent;
     int ttc;
     cin>>ttc;
+    char ch;
     array<int, ttc> a = {};
     for(int i=0;i<ttc;i++) cin>>array[i];
-    if (ch == 'a' || ch == 'e' || ch == 'i' ||
-                ch == 'o' || ch == 'u')
-                vowels++;
-            else
-                consonant++;
+    for(int i=0;i<ttc;i++){
+        ch = array[i];
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            vowel[i]++;
+        else
+            consonant[i]++;
+    }
+
 }
