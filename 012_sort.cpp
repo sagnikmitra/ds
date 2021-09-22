55e2ee201f9e3375c7a4f59d44e668a5f830006f
Sagnik Mitra <sagnikmitra123@gmail.com>
2021-09-22 23:53:21 +0530

    012 Sort

---
+++ 012_sort.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sort_012(int a[], int n){
+    continue;
+}
+int main(){
+    int a[];
+    int n; cin>>n;
+    for(int i=0;i<n; i++){
+        cin>>a[i];
+    }
+}
