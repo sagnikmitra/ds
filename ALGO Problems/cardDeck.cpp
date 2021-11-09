60101698b1d1c52222dfed827fb3a2db33a58275
Sagnik Mitra <sagnikmitra123@gmail.com>
2021-11-10 03:58:44 +0530

    IS-Ciphers Folder Relocation

--- ALGO Problems/cardDeck.cpp
+++ ALGO Problems/cardDeck.cpp
@@ -1,17 +1,21 @@
+//BS: TCS Digital
 #include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     deque<int> dq;
-    int n = 0; cin>>n;
-    for(int i =0; i<n; i++){
+    int n = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
         dq.push_back(i);
     }
-    while(dq.size() > 1) {
+    while (dq.size() > 1)
+    {
         dq.pop_front();
-        dq.push_back(sq.front());
+        dq.push_back(dq.front());
         dq.pop_front();
     }
-    cout<<dq.front();
+    cout << dq.front();
     return 0;
 }
