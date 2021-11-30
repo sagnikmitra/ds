17a54c861c8af04200d965be79fd49a84c07e6b4
Sagnik Mitra <sagnikmitra123@gmail.com>
2021-11-30 22:33:02 +0530

    Added MergeLL File

---
+++ LinkedList/mergeLL.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+int mergeTwoLinkedList()
+{
+    // Node *head1 = NULL;
+    // Node *head2 = NULL;
+    // Node *head3 = NULL;
+    // pushAtBeg(&head1, 1);
+    // pushAtBeg(&head1, 2);
+    // pushAtBeg(&head1, 3);
+    // pushAtBeg(&head1, 4);
+    // pushAtBeg(&head1, 5);
+    // pushAtBeg(&head2, 6);
+    // pushAtBeg(&head2, 7);
+    // pushAtBeg(&head2, 8);
+    // pushAtBeg(&head2, 9);
+    // pushAtBeg(&head2, 10);
+    // printLl(head1);
+    // printLl(head2);
+    // pushAtEnd(&head3, 1);
+    // pushAtEnd(&head3, 2);
+    // pushAtEnd(&head3, 3);
+    // pushAtEnd(&head3, 4);
+    // pushAtEnd(&head3, 5);
+    // pushAtEnd(&head3, 6);
+    // pushAtEnd(&head3, 7);
+    // pushAtEnd(&head3, 8);
+    // pushAtEnd(&head3, 9);
+    // pushAtEnd(&head3, 10);
+    // printLl(head3);
+    // inserAfterNode(&head3, 5, 11);
+    // printLl(head3);
+    // inserAfterNode(&head3, 5, 12);
+    // printLl(head3);
+    // inserAfterNode(&head3, 5, 13);
+    // printLl(head3);
+    // inserAfterNode(&head3, 5, 14);
+    // printLl(head3);
+    // inserAfterNode(&head3, 5, 15);
+    // printLl(head3);
+    // inserAfterNode(&head3, 5, 16);
+    // printLl(head3);
+    // inserAfterNode(&head3, 5, 17);
+    // printLl(head3);
+    // inserAfterNode(&head3, 5, 18);
+    // printLl(head3);}
